6f05bcfb3dde81f0eac8ea61d59dcd46fc66641d
ykan0052 <ykan0052@student.monash.edu>
2026-08-30 09:00:18 +0800

    Add notes for time complexity

--- Map/BFS.py
+++ Map/BFS.py
@@ -1,6 +1,7 @@
 from Map.Adjacency_list_map import MapGraph
 from Queue.linked_queue import LinkedQueue
-
+# If we use adjacency list, then the time comp is O(V+E)
+# If we use adjacency matrix, then the time comp is O(V^2)
 
 def bfs(graph, start_key):
     if start_key not in graph.vertices:
@@ -13,11 +14,12 @@ def bfs(graph, start_key):
     start_vertex = graph.vertices[start_key]
     visited.add(start_key)
     queue.append(start_vertex)
-
+    #list: O(V)
+    #matrix: O(v * V) = O(V^2)
     while not queue.is_empty():
         current_vertex = queue.serve()
         order.append(current_vertex.key)
-
+        #list: O(X1 + X2 + ...) = O(E), where X is the number of edges which one vertex have
         for edge in current_vertex.edges:
             neighbour = edge.to_vertex
 
